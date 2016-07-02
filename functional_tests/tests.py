d12cbf42511db47d6274e226b640d4c38a1294f7
enki-- <pbvinge@gmail.com>
2016-07-01 21:40:42 -1000

    first steps in FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -65,7 +65,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Francis visits teh home page.  There is no sign of Edith's
         # list
 
-        self.browser.get(self.life_server_url)
+        self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -88,5 +88,24 @@ class NewVisitorTest(LiveServerTestCase):
 
         # satisfied, they both go back to sleep
 
+    def test_layout_and_styling(self):
+    	# Edith goes to the home page
+    	self.browser.get(self.live_server_url)
+    	self.browser.set_window_size(1024, 768)
+
+    	# She notices the input box is nicely centered
+    	inputbox = self.browser.find_element_by_id('id_new_item')
+    	self.assertAlmostEqual(
+    		inputbox.location['x'] + inputbox.size['width'] / 2,
+    		512,
+    		delta=5
+    		)
+
+    	inputbox.send_keys('testing\n')
+    	inputbox = self.browser.find_element_by_id('id_new_item')
+    	self.assertAlmostEqual(
+    		inputbox.location['x'] + inputbox.size['width'] / 2,
+    		512,
+    		delta=5)
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
