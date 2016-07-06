624dcab8d9ede563d2d69f2bb04367e8355bcbb9
enki-- <pbvinge@gmail.com>
2016-07-05 16:06:15 -1000

    more validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,10 +3,11 @@
 from django.test import TestCase
 # from django.http import HttpRequest
 from django.utils.html import escape
+from unittest import skip
 
 from lists.models import Item, List
 # from lists.views import home_page
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm
 
 
 class HomePageTest(TestCase):
@@ -131,3 +132,15 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_dupicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
